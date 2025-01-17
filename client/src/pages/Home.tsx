b2cf1602ea41b487be5d7994cda0849f85588d18
Jam Samperio <jam.letsam93@gmail.com>
2025-01-16 20:20:20 -0600

    add card component

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Card from "../components/Card";
+
+const Home = () => {
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Card title="Card 1" description="This is the first card." imageUrl="https://via.placeholder.com/150" />
+    </>
+  );
+}
+
+export default Home;
