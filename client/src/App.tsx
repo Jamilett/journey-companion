708782f72db5b1d630b0de36585c79de7012ef1d
Jam Samperio <jam.letsam93@gmail.com>
2025-01-22 20:36:40 -0600

    add Login logic and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,37 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Homepage from "./components/HomePage";
-import Services from "./components/Services";
+import { Route, Routes } from "react-router-dom";
 import Activities from "./components/Activities";
+import Header from "./components/Header";
+import Services from "./components/Services";
+import HomePage from "./pages/Home";
+import Login from "./pages/Login";
 
 const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <div>
-      <Header />
+      {isLoggedIn && <Header onLogout={handleLogout} />}
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/activities" element={<Activities />} />
+        <Route path="/"
+          element={<Login onLogin={handleLogin} />} />
+        <Route path="/home"
+          element={isLoggedIn ? <HomePage /> : <Login onLogin={handleLogin} />} />
+        <Route path="/services"
+          element={isLoggedIn ? <Services /> : <Login onLogin={handleLogin} />} />
+        <Route path="/activities"
+          element={isLoggedIn ? <Activities /> : <Login onLogin={handleLogin} />} />
       </Routes>
     </div>
   );
 };
 
 
-export default App;
+export default App
